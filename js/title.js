137460ee7ad928a1453d7b80635489dabc661811
WouterSoethout <103152395+WouterSoethout@users.noreply.github.com>
2022-04-07 00:49:16 +0200

    Add files via upload

---
+++ js/title.js
@@ -0,0 +1,66 @@
+﻿if(document.addEventListener){
+	document.addEventListener("DOMContentLoaded", function(){
+		loaded();
+	});
+} else if(document.attachEvent){
+	document.attachEvent("onreadystatechange", function(){
+		loaded();
+	});
+}
+
+function loaded(){
+	
+	setInterval(loop, 350);
+
+}
+
+var x = 0;
+
+var titleText = [ "|",
+"O",
+"O|",
+"O |/",
+"O U",
+"O U|",
+"O U |/",
+"O U T",
+"O U T|",
+"O U T |/",
+"O U T E",
+"O U T E|",
+"O U T E |/",
+"O U T E R",
+"O U T E R|",
+"O U T E R |/",
+"O U T E R | S",
+"O U T E R | S|",
+"O U T E R | S |/",
+"O U T E R | S P",
+"O U T E R | S P|",
+"O U T E R | S P |/",
+"O U T E R | S P A",
+"O U T E R | S P A|",
+"O U T E R | S P A |/",
+"O U T E R | S P A C",
+"O U T E R | S P A C|",
+"O U T E R | S P A C |/",
+"O U T E R | S P A C E|/",
+"O U T E R | S P A C E",
+"O U T E R | S P A C E|",
+"O U T E R | S P A C E |/",
+
+"🚀",
+"* 🚀",
+"* * 🚀",
+"* * * 🚀",
+"* * * * 🚀   🌕",
+"* * * * *🚀 🌕",
+"* * * * * *🚀🌕",
+"* * * * * * *💥"];
+
+
+function loop(){
+
+	document.getElementsByTagName("title")[0].innerHTML = titleText[x++%titleText.length];
+
+}
